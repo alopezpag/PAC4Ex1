b947eccc4b6b86da5d847f50188ae7a424470b03
Arnau López <108700238+alopezpag@users.noreply.github.com>
2023-11-29 20:01:51 +0100

    edited method setBirthName

--- src/main/java/edu/uoc/pac4/Superstar.java
+++ src/main/java/edu/uoc/pac4/Superstar.java
@@ -1,6 +1,7 @@
 package edu.uoc.pac4;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Superstar {
     public static final int BIRTH_NAME_MIN_LENGTH = 1;
@@ -12,26 +13,51 @@ public class Superstar {
     public static final int HEIGHT_MIN_VALUE = 100;
     public static final int WEIGHT_MIN_VALUE = 30;
 
-    private String birthName = "Anonymous";
-    //private LocalDate birthDate = "New York";
-    private LocalDate birthDate = LocalDate.parse("New York");
+    private String birthName;
+    private LocalDate birthDate;
     private String birthPlace;
+    private Gender gender;
     private double height = 168;
     private double weight = 54;
-    private String ringName = "Superstar";
-    private Gender gender = Gender.FEMALE;
+    private String ringName;
 
-    public Superstar() {}
+    public Superstar() {
+        birthName = "Anonymous";
+        birthDate = LocalDate.now().minus(Period.ofDays(1));
+        gender = Gender.FEMALE;
+        height = 168;
+        weight = 54;
+        ringName = "Superstar";
+    }
 
     public Superstar(String birthName, LocalDate birthDate, String birthPlace, double height, double weight, String ringName) {
-
+        try {
+            setBirthName(birthName);
+            setBirthDate(birthDate);
+            setGender(gender);
+            setHeight(height);
+            setWeight(weight);
+            setRingName(ringName);
+        } catch (Exception e) {
+            System.out.println("test " + e.getMessage());
+        }
     }
 
     public String getBirthName() {
         return birthName;
     }
 
-    public void setBirthName(String birthName) {
+    public void setBirthName(String birthName) throws SuperstarException {
+        if (birthName.length() < BIRTH_NAME_MIN_LENGTH || birthName.length() > BIRTH_NAME_MAX_LENGTH) {
+            throw new SuperstarException(SuperstarException.MSG_ERR_BIRTH_NAME_LENGTH);
+        }
+
+        for (char c : birthName.toCharArray()) {
+            if (Character.isDigit(c)) {
+                throw new SuperstarException(SuperstarException.MSG_ERR_BIRTH_NAME_NUMBERS);
+            }
+        }
+
         this.birthName = birthName;
     }
 
